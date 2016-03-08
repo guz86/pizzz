502ea81e2a88ff9a8de4e3fef55a590ea1bf0214
Pavel Gordienko <guzmo6@rambler.ru>
2016-03-08 04:37:07 +0300

    add RakeFile upd GemFile add model

--- public/app.js
+++ public/app.js
@@ -1,11 +1,11 @@
 function something()
 {
 
-	var x = window.localStorage.getItem('bbb');
+	var x = window.localStorage.getItem('aaa');
 
 	x = x * 1 + 1;
 
-	window.localStorage.setItem('bbb', x);
+	window.localStorage.setItem('aaa', x);
 
 	alert(x);
 }	
