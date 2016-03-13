3936266f7615a3f2d5036c40019d62a3c9773f65
Pavel Gordienko <guzmo6@rambler.ru>
2016-03-13 21:24:40 +0300

    ({ )} in migrate

--- db/migrate/20160308020819_add_products.rb
+++ db/migrate/20160308020819_add_products.rb
@@ -1,7 +1,36 @@
 class AddProducts < ActiveRecord::Migration
   def change
-  	Product.create :title => 'Spicy pizza', :descrition => 'This is very spizy pizza', :price => 500, :size => 30, :is_spicy => true, :is_veg => false, :is_best_offer => true, :path_to_image => '/images/spice.jpg'
-  	Product.create :title => 'Pepperoni', :descrition => 'Nice and tasty pizza', :price => 700, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/images/pipperoni.JPG'
-  	Product.create :title => 'Vegetarian', :descrition => 'This is best and amazing pizza', :price => 300, :size => 30, :is_spicy => false, :is_veg => true, :is_best_offer => false, :path_to_image => '/images/veger.jpg'
+  	Product.create ({
+  		:title => 'Spicy pizza', 
+  		:descrition => 'This is very spizy pizza', 
+  		:price => 500, 
+  		:size => 30, 
+  		:is_spicy => true, 
+  		:is_veg => false, 
+  		:is_best_offer => true, 
+  		:path_to_image => '/images/spice.jpg'
+  		})
+
+  	Product.create ({
+  		:title => 'Pepperoni', 
+  		:descrition => 'Nice and tasty pizza', 
+  		:price => 700, 
+  		:size => 30, 
+  		:is_spicy => false, 
+  		:is_veg => false, 
+  		:is_best_offer => false, 
+  		:path_to_image => '/images/pipperoni.JPG'
+  		})
+
+  	Product.create ({
+  		:title => 'Vegetarian', 
+  		:descrition => 'This is best and amazing pizza', 
+  		:price => 300, 
+  		:size => 30, 
+  		:is_spicy => false, 
+  		:is_veg => true, 
+  		:is_best_offer => false, 
+  		:path_to_image => '/images/veger.jpg'
+  		})
   end
 end
