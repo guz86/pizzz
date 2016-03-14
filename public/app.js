bf19d5bd3af1a45e661cf182c89fa7b1d95d1aa0
Pavel Gordienko <guzmo6@rambler.ru>
2016-03-14 11:08:46 +0300

    summ items

--- public/app.js
+++ public/app.js
@@ -9,8 +9,20 @@
 function add_to_cart(id)
 {
 	var key = 'product_' + id;
-	
+
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+
+// считаем количество товаров в корзине
+var items = 0;
+
+for(var i in localStorage)
+{
+	localStorage[i]
+	items = items * 1 + localStorage[i] * 1
+}
+
+    console.log(items);
+
 }
