cbe9e45860165f64f5bef619f81072e26b63e35f
Pavel Gordienko <guzmo6@rambler.ru>
2016-03-14 10:20:13 +0300

    add product js

--- public/app.js
+++ public/app.js
@@ -1,16 +1,14 @@
-function something()
-{
-
-	var x = window.localStorage.getItem('aaa');
-
-	x = x * 1 + 1;
-
-	window.localStorage.setItem('aaa', x);
-
-	alert(x);
-}
+//function something()
+//{
+//	var x = window.localStorage.getItem('aaa');
+//	x = x * 1 + 1;
+//	window.localStorage.setItem('aaa', x);
+//	alert(x);
+//}
 
 function add_to_cart(id)
 {
-	alert('You added pizza with id: ' + id)
+	var x = window.localStorage.getItem('product_' + id);
+	x = x * 1 + 1;
+	window.localStorage.setItem('product_' + id, x);
 }
