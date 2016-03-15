79259dddee8d7c885cb754116787a6822eef2261
Pavel Gordienko <guzmo6@rambler.ru>
2016-03-15 17:52:18 +0300

    function cart_get_orders

--- public/app.js
+++ public/app.js
@@ -14,7 +14,7 @@ function add_to_cart(id)
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
 
-	alert ('Items in your cart: ' + cart_get_item())
+	alert ('Items in your cart:  ' + cart_get_item())
 }
 
 // считаем именно количество товаров по "product_" в корзине через функцию
@@ -33,4 +33,23 @@ function cart_get_item()
 		}
 	}
     return cnt;
+}
+
+// делаем строчку с заказом
+function cart_get_orders()
+{
+	var orders = '';
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // ключ
+		 var value = window.localStorage.getItem(key); // значение по ключу
+
+		if(key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ',';
+		}
+	}
+    return orders;
+    // возвращает product_1=1,product_3=2,
 }
