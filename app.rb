daf1ed78e8600f342483123d170d2570e64b44ff
Pavel Gordienko <guzmo6@rambler.ru>
2016-03-16 10:50:15 +0300

    arr on card.erb view

--- app.rb
+++ app.rb
@@ -19,7 +19,25 @@ get '/about' do
 end
 
 post '/cart' do
-	@o = params[:orders]
-
+	orders_input = params[:orders]
+	@orders = parse_order_line orders_input
 	erb :cart
+end
+
+
+def parse_order_line orders_input
+	# данные о заказе прилитают сюда в виде: product_1=4,product_2=8,product_3=3,
+	s1 = orders_input.split(',')
+	# получаем массив с product_1=4
+	arr = []
+	s1.each do |x|
+		s2 = x.split('=')
+		s3 = s2[0].split('_')
+		prod_id = s3[1]
+		#o = Product.find(prod_id)
+		amount = s2[1]
+		arr2 = [prod_id, amount]
+		arr.push arr2
+	end
+	return arr
 end
